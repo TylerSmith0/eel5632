ee7d41b3e61b8bdbbce616d60e15af5757831527
Tyler Smith <smithtylern@gmail.com>
2023-03-21 22:28:17 -0400

    init again

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
     
+@app.route("/floop")
+def floopy():
+    return "Floopy doopy"
 
 
 if __name__ == "__main__":
