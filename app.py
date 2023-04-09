ea7a91ee59d27d585c2bfc7c3069f1232262ea15
Tyler Smith <smithtylern@gmail.com>
2023-04-08 21:11:23 -0400

    logging change

--- app.py
+++ app.py
@@ -22,8 +22,7 @@ from models import Sensor
 
 ###################################################################
 load_dotenv()
-logging.basicConfig(filename=os.environ.get("LOGGING_FILE_LOC_APP"),
-                    filemode='a',
+logging.basicConfig(
                     level=logging.INFO,
                     format='%(asctime)s  |  %(name)s - %(levelname)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
